db8cf84dc2de906d497462f79bae8e36f989ee3e
Glitch (youspoon) <none>
2019-07-31 22:24:11 +0000

    Updated UI

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const gen = require('sentencer');
+const gen = require("sentencer");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
 });
 
-app.get('/get', function (request, response) {
-    response.send(gen.make('{{ adjective }} {{ noun }}'));
+app.get("/css", function(request, response) {
+  response.sendFile(__dirname + "/index.css");
 });
 
-const listener = app.listen(process.env.PORT, function () {
-
+app.get("/js", function(request, response) {
+  response.sendFile(__dirname + "/index.js");
 });
+
+app.get("/get", function(request, response) {
+  response.send(gen.make("{{ adjective }} {{ noun }}"));
+});
+
+const listener = app.listen(process.env.PORT || 3000, function() {});
