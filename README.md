60320ed012296e4c7712a3a29bb6c549c92395e4
Noah Richardson <ngregrichardson@gmail.com>
2019-08-01 09:32:24 -0400

    Updated README, added CORS accessibility

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A simple insult generator
+# You Spoon Insult Generator
+You Spoon is a simple random insult generator. It utilizes the [sentencer](https://www.npmjs.com/package/sentencer) npm package to get two random words and put them together.
+
+## API
+To get an insult, send a `GET` request to `https://youspoon.nrdesign.xyz/get` and an insult `string` will be returned in the body of the request.
--- youspoon.js
+++ youspoon.js
@@ -3,6 +3,14 @@ const app = express();
 const gen = require("sentencer");
 
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/index.html");
